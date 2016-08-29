288d3b62d386876ba78d6b5f2e5bb0c9e657cc08
cherry_linkloving <461807588@qq.com>
2016-08-29 14:30:01 +0800

    Special fields

--- addons/openacademy/models.py
+++ addons/openacademy/models.py
@@ -2,7 +2,8 @@
 
 from openerp import models, fields, api
 
-# class openacademy(models.Model):
-#     _name = 'openacademy.openacademy'
+class openacademy(models.Model):
+    _name = 'openacademy.openacademy'
 
-#     name = fields.Char()
+    name = fields.Char(string="Title", required=True)
+    description = fields.Text()
