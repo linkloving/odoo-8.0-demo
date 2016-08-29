4cb405cf245002861c1113a5dc11293749e95669
cherry_linkloving <461807588@qq.com>
2016-08-29 15:33:25 +0800

    Special fields

--- addons/openacademy/models.py
+++ addons/openacademy/models.py
@@ -2,8 +2,10 @@
 
 from openerp import models, fields, api
 
-class openacademy(models.Model):
-    _name = 'openacademy.openacademy'
+
+# 提供给demo.xml设置属性
+class Course(models.Model):
+    _name = 'openacademy.course'
 
     name = fields.Char(string="Title", required=True)
     description = fields.Text()
