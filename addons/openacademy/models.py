cec7eb0855985037027a128563228f5e37da260c
cherry_linkloving <461807588@qq.com>
2016-08-30 13:02:56 +0800

    Relational fields

--- addons/openacademy/models.py
+++ addons/openacademy/models.py
@@ -11,21 +11,24 @@ class Course(models.Model):
     '''
     name = fields.Char(string="Title", required=True)
     description = fields.Text(string="description")
-
-    responsible_id = fields.Many2one('res.user', ondelete='set null', string="Responsible", index=True)
+    # 一个责任人 有多个course
+    responsible_id = fields.Many2one('res.user', ondelete='set null', string="责任人", index=True)
+    # 一个course 有多个session
     session_ids = fields.One2many('openacademy.session', 'course_id', string="Sessions")
 
 
 # 会议表单
 class Session(models.Model):
-    _name ='openacademy.session'
+    _name = 'openacademy.session'
 
-    name = fields.Char(required=True)
+    name = fields.Char(string="Session Title", required=True)
     start_data = fields.Date()
     # digits(6 ,2) 共6位 小数点后只有2位
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="座位号")
-
+    # 每个session只能有一个Instructor
     instructor_id = fields.Many2one('res.partner', string="Instructor")
+    # 每个session只能有一个Course
     course_id = fields.Many2one('openacademy.course', ondelte='cadcade', string="Course", required=True)
-    attendee_ids = fields.Many2many('res.partner', string="Attendees")
+    # 每个session只能有多个参与者 每个参与者也可以参加其他session
+    attendee_ids = fields.Many2many('res.partner', string="参与者")
