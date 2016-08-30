648b2f37a3fb9224b5910c9ba392d290c09cf6e1
cherry_linkloving <461807588@qq.com>
2016-08-30 08:14:38 +0800

    Special fields

--- addons/openacademy/models.py
+++ addons/openacademy/models.py
@@ -6,6 +6,8 @@ from openerp import models, fields, api
 # 提供给demo.xml设置属性
 class Course(models.Model):
     _name = 'openacademy.course'
-
-    name = fields.Char(string="Title", required=True)
-    description = fields.Text()
+    '''
+    string 是在UI上显示的标题 若不设置则默认为声明的变量（例如'description'）
+    '''
+    name = fields.Char(string="标题", required=True,help="请输入标题")
+    description = fields.Text(string="描述")
