4cf5746eaf4192f4e665bb8dd430834f6746f685
cherry_linkloving <461807588@qq.com>
2016-08-31 16:09:00 +0800

    Relational fields

---
+++ myaddons/mymodule/controllers/main.py
@@ -0,0 +1,8 @@
+# # -*- coding: utf-8 -*-
+# from openerp import http
+#
+#
+# class Mymodule(http.Controller):
+#     @http.route('/mymodule/mymodule/', auth='public')
+#     def index(self, **kw):
+#         return "Hello World"
